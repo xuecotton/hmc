ab00fdf759a58660e9a98e5f586cd8e8425f28b1
sylj <439849135@qq.com>
2020-06-09 21:09:20 +0800

    调整登录方式的css

--- hmc/src/views/Register.vue
+++ hmc/src/views/Register.vue
@@ -1,16 +1,39 @@
 <template>
    <div id="register">
        <div class="Test">
-       <img src="../assets/images/111.png" alt="">
+       <img class="bg-img" src="../assets/images/111.png" alt="">
        <div class="mengban"></div>
        </div>
       <h1>欢迎来到花明村</h1>
-      <mt-button size="large">账户登录</mt-button>
-      <mt-button size="large">创建新账户</mt-button>
+      <router-link to="/" style="text-decoration:none">
+      <mt-button size="large" class="buttton_1"  >
+       
+     创建新账户
+      </mt-button>
+
+      </router-link>
+      <router-link to="/login" style="text-decoration:none" >
+      <mt-button size="large"  class="buttton_2">用户账号登录</mt-button>
+      </router-link>
       <p>或者</p>
-      <mt-button type="primary" size="large">用户账号登录</mt-button>
+     
+     <ul class="container">
+       <li><a href="">
+        <img src="../assets/images/weixin.png" alt="">   
+      </a></li>
+       <li><a href="">
+         <img src="../assets/images/xinlang.png" alt="">
+      </a></li>
+       <li><a href="">
+         <img src="../assets/images/zhifubao.png" alt="">
+         </a></li>
+       <li><a href="">
+         <img src="../assets/images/qq.png" alt="">
+         </a></li>
+     </ul>
    </div>
 </template>
+  
  <style scoped>
   .Test img{
         width: 100vw;
@@ -31,26 +54,26 @@
           rgba(255,255,255,1)
         );
       }
-      #register img{
+      #register .bg-img{
         
         width: 100%;
         height: 50%;
-        /* background: linear-gradient(to right bottom,rgba(0,0,0,0.9),rgba(0,0,0,0)),url(../assets/images/111.png) no-repeat center; */
+        background: linear-gradient(to right bottom,rgba(0,0,0,0.9),rgba(0,0,0,0)),url(../assets/images/111.png) no-repeat center;
       }
      #register h1 {
        text-align: center;
        color: #52c8cf;
        
      }
-      #register :nth-child(3){
+      .buttton_1{
         
        border: 1px solid  #52c8cf;
        color: #52c8cf;
        font: 1.5em sans-serif;
-       margin-bottom: 1.17%;
+       margin-bottom: 4%;
      }
 
-     #register :nth-child(4){
+     .buttton_2{
         
        border: 1px solid  #52c8cf;
        color: #52c8cf;
@@ -60,13 +83,22 @@
      }
      #register :nth-child(5){
       
-       margin-top:1.12%;
+       margin-top:3%;
        color: #7c808b;
        text-align: center;
      }
-     #register :nth-child(6){
-       margin-top: 1.17%;
-       
-       background:#52c8cf;
-     }
+
+     .container li{
+      display: inline-block;
+      margin-top: 2%;
+      margin-right:13.8%
+    } 
+    .container li a img{
+      width:40px;
+      /* height: 9.66%; */
+    }
+    .container li{
+      border: none;
+    }
  </style>
+  
