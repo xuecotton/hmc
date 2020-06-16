9fda780908495fdca1ef89d46fbaecea5f9d01ca
sylj <439849135@qq.com>
2020-06-16 20:20:28 +0800

    登录页界面完成以及sessionstorage实现及后台交互

--- hmc/src/views/Login.vue
+++ hmc/src/views/Login.vue
@@ -15,7 +15,7 @@
         <mt-field
          class="mt_field"
          type="text"
-         @blur.native.capture="checkUsername"
+       
          v-model="username"
          
         >
@@ -26,19 +26,10 @@
          <mt-field
           class="mt_field"
          type="password"
-         @blur.native.capture="checkpassword"
+        
          v-model="password"
         >
-        <!-- </mt-field>
-         <label
-          class="mt_label">确认密码</label>
-         <mt-field
-         id="password"
-          class="mt_field"
-          type="password"
-           @blur.native.capture="checkconpassword"
-         v-model="conpassword" 
-        > -->
+       
         </mt-field>
         <button class="button" @click="login">登录</button>
       </div>
@@ -59,7 +50,39 @@ export default {
    },
    methods:{
      login(){
-        
+        // 检测用户名和密码的规范
+        var obj={
+          username:this.username,
+          password:this.password
+        };
+       this.axios.post('/login',this.qs.stringify(obj)).then(
+         (res)=>{
+           if(res.data.code==202){
+             this.$messagebox.alert('用户名或密码错误','登录提示');
+           }
+           if(res.data.code==200){
+            //  登录成功
+            this.$messagebox.alert('登录成功','登录提示');
+            // 将相关信息存储到sessionStorage中
+            sessionStorage.setItem('id',res.data.info.id);
+            sessionStorage.setItem('username',res.data.info.username);
+            sessionStorage.setItem('nickname',res.data.info.nickname);
+            sessionStorage.setItem('avatar',res.data.info.avatar);
+             sessionStorage.setItem('realname',res.data.info.realname);
+              sessionStorage.setItem('phone',res.data.info.phone);
+           sessionStorage.setItem('isLogined',true);
+           }
+          /******后期要改
+          //  将数据存储到Vuex中
+          this.$store.commit('userLogined',res.data.info);
+          // 根据地址栏中是否存在redirect的参数来决定跳转到哪个路由
+          if(this.$route.query.redirect){
+            this.$router.push(this.$route.query.redirect);
+          }else{
+            this.$router.push('/')
+          }
+          *********** */
+         }); 
      }
    }
   
